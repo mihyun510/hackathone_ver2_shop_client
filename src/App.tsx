c6e60bcfbb5a7c90f2637d5125a3aed7032e8052
kmh <mihyun510@github.com>
2025-02-20 22:43:59 +0900

    add login navigate error 수정

--- src/App.tsx
+++ src/App.tsx
@@ -3,36 +3,44 @@ import {
   Routes,
   Route,
   Navigate,
+  useLocation,
 } from "react-router-dom";
-import { AuthProvider } from "./context/AuthContext";
+import { AuthProvider, useAuth } from "./context/AuthContext";
 import LoginPage from "./pages/auth/LoginPage";
 import RegisterPage from "./pages/auth/RegisterPage";
 import HomePage from "./pages/main/HomePage";
 
+function RootApp() {
+  const { user, token } = useAuth();
+  const isLoginForm = ["login", "register"].includes(
+    useLocation().pathname.substring(1)
+  );
+  return (
+    <Routes>
+      {token && !isLoginForm ? (
+        <>
+          <Route path="/home" element={<HomePage />} />
+          <Route path="*" element={<Navigate to="/home" />} />
+        </>
+      ) : (
+        <>
+          <Route path="/login" element={<LoginPage />} />
+          <Route path="/register" element={<RegisterPage />} />
+          <Route path="*" element={<Navigate to="/login" />} />
+        </>
+      )}
+    </Routes>
+  );
+}
+
 const App = () => {
-  //const { user } = useAuth();
-  const token = localStorage.getItem("token");
+  //const token = localStorage.getItem("token");
   //console.log(token);
 
   return (
     <Router>
       <AuthProvider>
-        <Routes>
-          {!token ? (
-            <>
-              <Route path="/login" element={<LoginPage />} />
-              <Route path="/register" element={<RegisterPage />} />
-              {/* No routes matched location '/home' 오류 해결 무조건 있어야되나보나 토큰이 없는 경우 존재하지 않는 url로 접근했을경우 저리 로직 */}
-              <Route path="*" element={<Navigate to="/login" />} />
-            </>
-          ) : (
-            <>
-              <Route path="/home" element={<HomePage />} />
-              {/* 존재하지 않는 url로 이동하면 /로 이동함. */}
-              <Route path="*" element={<Navigate to="/home" />} />
-            </>
-          )}
-        </Routes>
+        <RootApp />
       </AuthProvider>
     </Router>
   );
